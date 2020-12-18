72bb974c2cf8b8344904762108a7f9402762d0b4
Anna Korunska <akorunska@gmail.com>
2020-12-18 14:14:38 +0200

    eslint configuration

--- patterns/singleton/database.js
+++ patterns/singleton/database.js
@@ -1,11 +1,10 @@
 const connect = (credentials) => {
-  if (credentials === 'secret key from env variables') {
-    return {
-      connection: 'This db is now connected',
-    };
-  } else {
-    throw 'Invalid credentials';
+  if (credentials !== 'secret key from env variables') {
+    throw new Error('Invalid credentials');
   }
+  return {
+    connection: 'This db is now connected',
+  };
 };
 
 export default connect;
