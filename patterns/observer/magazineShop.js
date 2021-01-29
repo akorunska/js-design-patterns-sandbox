6e8cb86e331691c039e9600ada3d0b60bbb77b56
Anna Korunska <akorunska@gmail.com>
2021-01-29 17:45:17 +0200

    added magazine shop to demonstrate possibilies of the publisher observer on an example

---
+++ patterns/observer/magazineShop.js
@@ -0,0 +1,34 @@
+import MagazinePublisher from './magazinePublisher';
+import Customer from './customer';
+
+class MagazineShop {
+  constructor() {
+    this.publishers = {};
+  }
+
+  addPublisher(publisherId, publisher) {
+    this.publishers[publisherId] = publisher;
+  }
+
+  subscribe(publisherId, user) {
+    this.publishers[publisherId].addObserver(user);
+  }
+}
+const voguePublisher = new MagazinePublisher();
+const shop = new MagazineShop();
+shop.addPublisher('Vogue', voguePublisher);
+
+const larry = new Customer('Larry');
+const jane = new Customer('Jane');
+
+shop.subscribe('Vogue', larry);
+voguePublisher.publishNewMagazineNumber();
+console.log(
+  `larry: ${larry.lastReceivedMagazine}, jane: ${jane.lastReceivedMagazine}`
+);
+
+shop.subscribe('Vogue', jane);
+voguePublisher.publishNewMagazineNumber();
+console.log(
+  `larry: ${larry.lastReceivedMagazine}, jane: ${jane.lastReceivedMagazine}`
+);
